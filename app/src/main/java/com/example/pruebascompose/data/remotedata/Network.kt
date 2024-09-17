f837bc292de4423d1445d2b8941e7296e5027266
juancarlos <jccm697@gmail.com>
2024-09-17 19:38:44 +0200

    feat(api): first petition

--- app/src/main/java/com/example/pruebascompose/data/remotedata/Network.kt
+++ app/src/main/java/com/example/pruebascompose/data/remotedata/Network.kt
@@ -1,4 +1,14 @@
 package com.example.pruebascompose.data.remotedata
 
-class Network {
+import com.example.pruebascompose.data.api.MoviesApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Network {
+   val retrofit = Retrofit.Builder()
+       .baseUrl("https://api.themoviedb.org/3/")
+       .addConverterFactory(GsonConverterFactory.create())
+       .build()
+
+    val apiService = retrofit.create(MoviesApi::class.java)
 }
